028cec44eb7ac046d4dd18e0d86c31695b2e9f59
Kevin Downey <redchin@gmail.com>
2014-09-05 22:32:49 -0700

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.manigfeald/kvgc "0.1.0-SNAPSHOT"
+(defproject com.manigfeald/kvgc "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
